06e2cc48394480bf68efc9d34a94b9784af449c1
Lokesh Soni (TMS) <lokesh.soni@toyota.com>
2020-07-31 00:24:34 +0530

    Fri Jul 31 00:24:34 IST 2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_analytics/observer.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'services/services.dart';
-import 'screens/screens.dart';
 import 'package:provider/provider.dart';
-
-void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,37 +9,7 @@ class MyApp extends StatelessWidget {
         StreamProvider<Report>.value(stream: Global.reportRef.documentStream),
         StreamProvider<FirebaseUser>.value(stream: AuthService().user),
       ],
-      child: MaterialApp(
-        // Firebase Analytics
-        navigatorObservers: [
-          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
-        ],
-
-        // Named Routes
-        routes: {
-          '/': (context) => LoginScreen(),
-          '/topics': (context) => TopicsScreen(),
-          '/profile': (context) => ProfileScreen(),
-          '/about': (context) => AboutScreen(),
-        },
-
-        // Theme
-        theme: ThemeData(
-          fontFamily: 'Nunito',
-          bottomAppBarTheme: BottomAppBarTheme(
-            color: Colors.black87,
-          ),
-          brightness: Brightness.dark,
-          textTheme: TextTheme(
-            body1: TextStyle(fontSize: 18),
-            body2: TextStyle(fontSize: 16),
-            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
-            headline: TextStyle(fontWeight: FontWeight.bold),
-            subhead: TextStyle(color: Colors.grey),
-          ),
-          buttonTheme: ButtonThemeData(),
-        ),
-      ),
+      child: MaterialApp(),
     );
   }
 }
